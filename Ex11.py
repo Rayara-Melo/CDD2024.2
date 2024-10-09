7095da21c5f88bc1b3e936b8e9e4e506d4d621f1
Rayara-Melo <rayaramello@gmail.com>
2024-10-09 20:51:50 -0300

    Update Ex11.py

--- Ex11.py
+++ Ex11.py
@@ -1,4 +1,9 @@
 #FAÇA UMA FUNÇÃO QUE CONTE QUANTAS VOGAIS TEM UM TEXTO.
 # #TEXTO: O rato roeu a roupa do Rei de Roma
 
-from Biblioteca import vogal
+n = input("Insira o texto: ")
+soma = 0
+for x in n:
+        if x in "aeiouAEIOU":
+            soma += 1
+print(f"O texto informado contém {soma} vogais")
