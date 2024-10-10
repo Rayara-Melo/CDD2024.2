93dbcd87b394559ba822589db4d83443f5059290
Rayara-Melo <rayaramello@gmail.com>
2024-10-10 11:16:12 -0300

    Add files via upload

--- biblioteca.py
+++ biblioteca.py
@@ -1,11 +1,18 @@
 class Pessoa:
-    def __init__(self, nome, peso, idade): #TUDO QUE TEM sef É UM ATRIBUTO.
-        self.nome=nome
-        self.peso=peso
-        self.idade=idade
+    def __init__(self, nome, peso, idade, falar=False, comer=False, dormir=False): #TUDO QUE TEM sef É UM ATRIBUTO.
+        self.nome = nome
+        self.peso = peso
+        self.idade = idade
+        self.falar = falar
+        self.comer = comer
+        self.dormir = dormir
+
+    def falar (self):
+        print(f"{self.nome} está falando")
+        self.falar == True
     def comer (self):
-        print(f"{self.nome} foi comer")
+        print(f"{self.nome} está comendo")
+        self.comer == True
     def dormir (self):
-        print(f"{self.nome} foi dormir")
-    def andar (self):
-        print(f"{self.nome} foi andar")
+        print(f"{self.nome} está dormindo")
+        self.dormir == True
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from biblioteca import *
 
 p1 = Pessoa("João", 75, 21)
+p1.falar()
 
-print(p1.nome)
-p1.comer()
-p1.dormir()
-p1.andar()
+print(p1.falar)
